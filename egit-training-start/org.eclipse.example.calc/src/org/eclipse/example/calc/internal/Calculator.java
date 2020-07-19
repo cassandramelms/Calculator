9e102be24cfee4d1e0349f956c3cb41b835720ca
1654879_snhu <1654879_snhu@NV-SNHU4-RDS6.apporto.com>
2020-07-19 18:42:37 -0400

    Add power class

--- egit-training-start/org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ egit-training-start/org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -17,6 +17,7 @@ import org.eclipse.example.calc.internal.operations.Divide;
 import org.eclipse.example.calc.internal.operations.Equals;
 import org.eclipse.example.calc.internal.operations.Minus;
 import org.eclipse.example.calc.internal.operations.Plus;
+import org.eclipse.example.calc.internal.operations.Power;
 import org.eclipse.example.calc.internal.operations.Square;
 
 public class Calculator {
@@ -41,6 +42,7 @@ public class Calculator {
 		new Equals();
 		new Minus();
 		new Plus();
+		new Power();
 		new Square();
 	}
 
