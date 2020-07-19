b1bea7bddf870eba4fa9567a68432cc615f37eb0
1654879_snhu <1654879_snhu@NV-SNHU4-RDS6.apporto.com>
2020-07-19 18:13:20 -0400

    add Divide Operation

--- egit-training-start/org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ egit-training-start/org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -13,6 +13,7 @@ import org.eclipse.example.calc.BinaryOperation;
 import org.eclipse.example.calc.Operation;
 import org.eclipse.example.calc.Operations;
 import org.eclipse.example.calc.UnaryOperation;
+import org.eclipse.example.calc.internal.operations.Divide;
 import org.eclipse.example.calc.internal.operations.Equals;
 import org.eclipse.example.calc.internal.operations.Minus;
 import org.eclipse.example.calc.internal.operations.Plus;
@@ -36,6 +37,7 @@ public class Calculator {
 	}
 
 	private void setupDefaultOperations() {
+		new Divide();
 		new Equals();
 		new Minus();
 		new Plus();
